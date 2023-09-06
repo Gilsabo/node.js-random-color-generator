6002b8ec3fdf71ddd5f8899779967687da6c4ff2
Gilsabo <gilsala_88@hotmail.com>
2023-09-06 07:26:20 +0200

    Add comments clarify what the color library does

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { argv } from 'node:process';
-import chalk from 'chalk';
-import randomHexColor from 'random-hex-color';
+import { argv } from 'node:process'; // helps get input user in CLI
+import chalk from 'chalk'; // prints color with hex format
+import Color from 'color'; // prints color with hsl format
+import randomHexColor from 'random-hex-color'; // generates randomcolor in hex format
 
 const hashFrame = ` ###############################
  ###############################
@@ -12,11 +13,20 @@ const hashFrame = ` ###############################
  ###############################
  ###############################`;
 
-// console.log(chalk.hex(randomHexColor())(hashFrame));
-console.log(argv);
+const hueStringUser = argv[2];
+const luminosityStringUser = argv[3];
 
+const hueStringUserToRGBFormat = Color(hueStringUser);
+
+console.log(argv);
+console.log(hueStringUserToRGBFormat.hsl().color[2]); // converts color to hsl format and access lightness
+console.log(hueStringUser);
+console.log(hueStringUserToRGBFormat);
+console.log(luminosityStringUser);
+
+// to be done
 if (argv[2] === 'red' || argv[2] === 'green' || argv[2] === 'blue') {
-  console.log(chalk.red(hashFrame));
+  console.log(chalk.blue(hashFrame));
 } else {
   console.log(chalk.hex(randomHexColor())(hashFrame));
 }
